7724cd1350a09b4dee51e748d38b84f5488e434e
Bartosz Radwan <b.radwan@citypartner.pl>
2016-09-23 11:27:24 +0200

    Add device: Edge-Core switch ES3528MV2

--- includes/discovery/os/edge-core.inc.php
+++ includes/discovery/os/edge-core.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!$os) {
-    if (strstr($sysObjectId, '.1.3.6.1.4.1.259.6.10.94')) {
+    if (strstr($sysObjectId, '.1.3.6.1.4.1.259.6.10.94') || strstr($sysObjectId, '.1.3.6.1.4.1.259.10.1.22.101')) {
         $os = 'edge-core';
     }
 }
--- includes/polling/os/edge-core.inc.php
+++ includes/polling/os/edge-core.inc.php
@@ -1,5 +1,15 @@
 <?php
 
-$version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"), '"');
-$hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"), '"');
-$hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"), '"');
+$_sysObjectId = trim(snmp_get($device, "sysObjectID.0", "-OQvn", "SNMPv2-MIB"), '"');
+
+if (strstr($_sysObjectId, '.1.3.6.1.4.1.259.10.1.22.101')) {
+    #ES3528MV2
+    $version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MV2-MIB"), '"');
+    $hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MV2-MIB"), '"');
+    $hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"), '"');
+} elseif (strstr($_sysObjectId, '.1.3.6.1.4.1.259.6.10.94')) {
+    #ES3528M
+    $version = trim(snmp_get($device, "swOpCodeVer.1", "-OQv", "ES3528MO-MIB"), '"');
+    $hardware = "Edge-Core " . trim(snmp_get($device, "swProdName.0", "-OQv", "ES3528MO-MIB"), '"');
+    $hostname = trim(snmp_get($device, "sysName.0", "-OQv", "SNMPv2-MIB"), '"');
+}
